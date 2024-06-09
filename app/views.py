d21e9a95e2286bc38f50d1e91cb15c99c0b1e84e
Tekimg <jo.mansillag@duocuc.cl>
2024-06-09 01:14:05 -0400

    htmls y css, actualizacion urls y views, imagenes agregadas

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'app/home.html')
+
+def alimentos(request):
+    return render(request, 'app/alimentos.html')
